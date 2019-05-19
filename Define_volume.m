d458ac368ec64b55182032bd5f81be87952aa3a5
Yangguoyuan <37663318+Yangguoyuan@users.noreply.github.com>
2019-05-19 20:27:04 +0800

    Delete Define_volume.m

--- Define_volume.m
+++
@@ -1,9 +0,0 @@
-clc;
-clear;
-
-    sub_path='/guoyuan_data/make_template_hcp/show40_200/40/a_sub_average.nii';
-    if exist(sub_path,'file') 
-       mri_data=MRIread(sub_path);
-       mean_data=mean(mean(mean(mean(abs(mri_data.vol)))))*3;
-       all_deform(end+1)=mean_data;
-    end
